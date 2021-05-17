10c192d5c85452c7c5ce43c12eeacd9b018ac433
Paul <68764896+Pdiddy973@users.noreply.github.com>
2021-05-17 04:32:35 -0400

    holy shit it works now

--- commands/sonarr.js
+++ commands/sonarr.js
@@ -12,49 +12,51 @@ module.exports = {
 
         if (!imdb_query) return message.reply("please add a show to the command")
 
-        nameToImdb(`${imdb_query}`, function (err, res, inf) {
+        nameToImdb(`${imdb_query}`, function(err, res, inf) {
             sonarrid = inf.meta.id
             sonarrshow = inf.meta.name
             sonarryear = inf.meta.year
             sonarrtype = inf.meta.type
             sonarrimage = inf.meta.image.src
+            derpaf()
         })
-        let embed = new Discord.MessageEmbed()
-            .setColor('#00ccff')
-            .setTitle('Adding ${sonarrshow}-(${sonarryear}) to Sonarr')
-            .setURL('https://www.imdb.com/title/${sonarrid}/')
-            .setThumbnail('https://github.com/Sonarr/sonarr.github.io/blob/master/logo/sonarr-256.png')
-            //${sonarrimage}
-            .setDescription(`Make sure this looks correct before confirming with ${yes}\n\n`)
 
-        let messageEmbed = await message.channel.send(embed);
-        messageEmbed.react(yes);
-        messageEmbed.react(no);
+        async function derpaf() {
+            let embed = new Discord.MessageEmbed()
+                .setColor('#00ccff')
+                .setTitle(`Adding ${sonarrshow}-(${sonarryear}) to Sonarr`)
+                .setURL(`https://www.imdb.com/title/${sonarrid}/`)
+                .setThumbnail(`${sonarrimage}`)
+                //${sonarrimage}
+                .setDescription(`Make sure this looks correct before confirming with ${yes}\n\n`)
 
-        client.on('messageReactionAdd', async (reaction, user) => {
-            if (reaction.message.partial) await reaction.message.fetch();
-            if (reaction.partial) await reaction.fetch();
-            if (user.bot) return;
-            if (!reaction.message.guild) return;
+            let messageEmbed = await message.channel.send(embed);
+            messageEmbed.react(yes);
+            messageEmbed.react(no);
 
-            if (reaction.message.channel.id == channel) {
-                if (reaction.emoji.name === yes) {
-                    console.log(`${user} has selceted Correct`)
-                    console.log(`${sonarrshow}`)
+            client.on('messageReactionAdd', async (reaction, user) => {
+                if (reaction.message.partial) await reaction.message.fetch();
+                if (reaction.partial) await reaction.fetch();
+                if (user.bot) return;
+                if (!reaction.message.guild) return;
+
+                if (reaction.message.channel.id == channel) {
+                    if (reaction.emoji.name === yes) {
+                        console.log(`${user} has selceted Correct`)
+                        console.log(`${sonarrshow}`)
+                    }
+                    if (reaction.emoji.name === no) {
+                        console.log(`${user} has selceted Incorrect`)
+                        await message.channel.messages.fetch({
+                            limit: 2
+                        }).then(messages => {
+                            message.channel.bulkDelete(messages);
+                        });
+                    }
+                } else {
+                    return;
                 }
-                if (reaction.emoji.name === no) {
-                    console.log(`${user} has selceted Incorrect`)
-                    await message.channel.messages.fetch({
-                        limit: 2
-                    }).then(messages => {
-                        message.channel.bulkDelete(messages);
-                    });
-                }
-            } else {
-                return;
-            }
-
-        });
-
+            });
+        }
     }
 }
