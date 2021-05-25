9e189cff805456341820fa376d40d0f1c69cecad
Paul <68764896+Pdiddy973@users.noreply.github.com>
2021-05-24 22:20:50 -0400

    Update request.js

--- commands/request.js
+++ commands/request.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'request',
     description: 'Adds Shows to Sonarr Queue',
     async execute(client, message, args, Discord) {
-        const channel = process.env.REQUEST_CHANNEL;
+        const requestChannel = process.env.REQUEST_CHANNEL;
         const requestLog = process.env.REQUEST_LOG_CHANNEL
         const yes = '✅';
         const no = '🚫';
@@ -16,80 +16,81 @@ module.exports = {
         //Scrapes IMDB for data
         nameToImdb(`${imdb_query}`, function (err, res, inf) {
             id = inf.meta.id
-            names = inf.meta.name
+            media = inf.meta.name
             year = inf.meta.year
             type = inf.meta.type
             image = inf.meta.image.src
-            derpaf()
         })
 
-        async function derpaf() {
-            if (type === 'TV series') {
-                var color = '#00ccff'
-                var process = 'Sonarr'
-            }
-            if (type === 'feature') {
-                var color = '#ffc231'
-                var process = 'Radarr'
-            }
-
-            let embed = new Discord.MessageEmbed()
-                .setColor(`${color}`)
-                .setTitle(`Add ${names} - (${year}) to ${process}`)
-                .setURL(`https://www.imdb.com/title/${id}/`)
-                .setThumbnail(`${image}`)
-                .setDescription(`Make sure this looks correct before confirming with ${yes}\n\n`)
-
-            let messageEmbed = await message.channel.send(embed);
-            messageEmbed.react(yes);
-            messageEmbed.react(no);
-
-            client.on('messageReactionAdd', async (reaction, user) => {
-                if (reaction.message.partial) await reaction.message.fetch();
-                if (reaction.partial) await reaction.fetch();
-                if (user.bot) return;
-                if (!reaction.message.guild) return;
-                console.log('test1')
-                if (reaction.message.channel.id == channel) {
-                    console.log('test2')
-                    if (reaction.emoji.name === yes) {
-                        //log embed
-                        let logEmbed = new Discord.MessageEmbed()
-                            .setColor(`${color}`)
-                            .setTitle(`Requested ${names} - (${year}) to ${process}`)
-                            .setURL(`https://www.imdb.com/title/${id}/`)
-                            .setThumbnail(`${image}`)
-                            .setDescription(`${user} has Requested ${names}`)
-                        //Sonarr API
-                        if (type == 'TV series') {
-                            console.log('sent to sonarr')
-                        }
-                        //Radarr API
-                        if (type == 'feature') {
-                            console.log('sent to radarr')
-                        }
-                        //If Request log has a channel ID, send log
-                        if (!requestLog) {} else {
-                            client.channels.cache.get(requestLog).send(logEmbed);
-                        }
-                        //Remove From Request Channel
-                        await message.channel.messages.fetch({
-                            limit: 2
-                        }).then(messages => {
-                            message.channel.bulkDelete(messages);
-                        });
-                    }
-                    if (reaction.emoji.name === no) {
-                        await message.channel.messages.fetch({
-                            limit: 2
-                        }).then(messages => {
-                            message.channel.bulkDelete(messages);
-                        });
-                    }
-                } else {
-                    return;
-                }
-            });
+        if (type === 'TV series') {
+            var color = '#00ccff'
+            var process = 'Sonarr'
         }
+        if (type === 'feature') {
+            var color = '#ffc231'
+            var process = 'Radarr'
+        }
+
+        //Confirm Embed
+        let confirmEmbed = new Discord.MessageEmbed()
+            .setColor(`${color}`)
+            .setTitle(`Add ${media} - (${year}) to ${process}`)
+            .setURL(`https://www.imdb.com/title/${id}/`)
+            .setThumbnail(`${image}`)
+            .setDescription(`Make sure this looks correct before confirming with ${yes}\n\n`)
+        
+            //log embed
+        let logEmbed = new Discord.MessageEmbed()
+            .setColor(`${color}`)
+            .setTitle(`Requested ${media} - (${year}) to ${process}`)
+            .setURL(`https://www.imdb.com/title/${id}/`)
+            .setThumbnail(`${image}`)
+            .setDescription(`${user} has Requested ${media}`)
+        
+            //Send Confirm Embed and React
+        let messageEmbed = await message.channel.send(confirmEmbed);
+        messageEmbed.react(yes);
+        messageEmbed.react(no);
+        
+        //Run Based on Reaction
+        client.on('messageReactionAdd', async (reaction, user) => {
+            //Checks
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == requestChannel) {
+                if (reaction.emoji.name === yes) {
+                    //Sonarr API
+                    if (type == 'TV series') {
+                        console.log('sent to sonarr')
+                    }
+                    //Radarr API
+                    if (type == 'feature') {
+                        console.log('sent to radarr')
+                    }
+                    //If Request log has a channel ID, send log
+                    if (!requestLog) {} else {
+                        client.channels.cache.get(requestLog).send(logEmbed);
+                    }
+                    //Remove From Request Channel
+                    message.channel.messages.fetch({
+                        limit: 2
+                    }).then(messages => {
+                        message.channel.bulkDelete(messages);
+                    });
+                }
+                if (reaction.emoji.name === no) {
+                    message.channel.messages.fetch({
+                        limit: 2
+                    }).then(messages => {
+                        message.channel.bulkDelete(messages);
+                    });
+                }
+            } else {
+                return;
+            }
+        });
     }
 }
